23e967fbd12752ba57111a2a0329470bdf5d1e9f
Jaiden <38144027+jaidenlab@users.noreply.github.com>
2024-09-14 19:11:43 -0400

    Able to get timetable using only requests and bs4

--- main.py
+++ main.py
@@ -1,36 +1,61 @@
+import requests
 import sys
-
-
-
+import pyotp
+from bs4 import BeautifulSoup
 import logging
 
-# THIS WORKS!!!!
-# POST https://wd3.myworkday.com/wday/authgwy/cineplex/login-auth.xml
-# Headers:
-# X-Workday-Client = 2024.36.21
-# Content-Type = application/x-www-form-urlencoded
-# userName = 
-# password = 
-
-# Mutlifactor authentication (2FA)
-# GET https://wd3.myworkday.com/wday/authgwy/cineplex/api/authn/mfa/summary
-
-# POST https://wd3.myworkday.com/wday/authgwy/cineplex/api/authn/mfa/challenge/workday/totp
-# passcode = 629525
+import os
+from dotenv import load_dotenv
 
 # https://www.w3.org/TR/webauthn-2
 
+load_dotenv()
+
+username = os.environ["CINEPLEX_USERNAME"]
+password = os.environ["CINEPLEX_PASSWORD"]
+totp_secret = os.environ["CINEPLEX_TOTP_SECRET"]
+
 def main():
-    driver = webdriver.Chrome()
-    try:
-        # Navigate to cineplex workday login page
-        driver.get("https://wd3.myworkday.com/wday/authgwy/cineplex/login.htmld")
-        
+    session = requests.Session()
+    session.headers.update({
+        "X-Workday-Client": "2024.37.11"
+    })
 
-        pass
-    finally:
-        driver.quit()
+    # Returns JSON on success, XML on failure
+    response = session.post(
+        "https://wd3.myworkday.com/wday/authgwy/cineplex/login-auth.xml",
+        headers = {"Content-Type": "application/x-www-form-urlencoded"},
+        data = {"userName": username, "password": password}
+    )
 
+    json = response.json()
+    token = json["sessionSecureToken"]
+
+    # errorMessage: "You entered an incorrect code. Please try again."
+    # status: "MFA_CHALLENGE"
+    # result: "FAILURE"
+    response = session.post(
+        "https://wd3.myworkday.com/wday/authgwy/cineplex/api/authn/mfa/challenge/workday/totp",
+        headers = {
+            "Content-Type": "application/json",
+            "Session-Secure-Token": token,
+        },
+        json = {"passcode": pyotp.TOTP(totp_secret).now()}
+    )
+
+    response = session.get("https://wd3.myworkday.com/cineplex/samlsso/autosubmit/6503$1.htmld")
+    soup = BeautifulSoup(response.text, "html.parser")
+    print(soup.prettify())
+
+    # Since Javascript is not supported, parse and submit the form
+    inputs = {}
+    inputs.update({"RelayState": soup.find_all("input")[0]["value"]})
+    inputs.update({"SAMLResponse": soup.find_all("input")[1]["value"]})
+    print(inputs)
+
+    response = session.post("https://workbrain.cineplex.com/samlsso", headers={"Content-Type": "application/x-www-form-urlencoded"}, data=inputs)
+    response = session.get("https://workbrain.cineplex.com/etm/time/timesheet/etmTnsMonth.jsp?selectedTocID=181&parentID=10")
+    print(response.text)
 
 # https://docs.python.org/3/library/__main__.html
 if __name__ == "__main__":
