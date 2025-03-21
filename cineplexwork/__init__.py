1a99eee6ff81b40b3dc88e0708167819d9e1ee83
Jaiden Labelle <jaidenjlabelle@gmail.com>
2025-03-20 20:33:39 -0400

    Working docker container to automatically update schedule

--- cineplexwork/__init__.py
+++ cineplexwork/__init__.py
@@ -1,5 +1,5 @@
-from shift import Shift
-from schedule import Schedule
-from cineplex import Cineplex
+from cineplexwork.shift import Shift
+from cineplexwork.schedule import Schedule
+from cineplexwork.cineplex import Cineplex
 
 __all__ = ['Shift', 'Schedule', 'Cineplex']
